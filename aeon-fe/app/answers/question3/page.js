4f13a8288003768442af9c32155bbd9cb19e4e38
hardihtahir <hardih@strateqebs.onmicrosoft.com>
2023-09-20 08:10:11 +0800

    done with question 3

--- aeon-fe/app/answers/question3/page.js
+++ aeon-fe/app/answers/question3/page.js
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 
-const Question3 = () => {
+function Question3 (props) {
+  const twoSum = (numbers, target) => {
+    let index1 = 0;
+    let index2 = numbers.length - 1;
+    while (index1 < index2) {
+      const sum = numbers[index1] + numbers[index2];
+      if (sum === target) {
+        return [index1 + 1, index2 + 1];
+      } else if (sum < target) {
+        index1++;
+      } else {
+        index2--;
+      }
+    }
+    return [];
+  }
+  useEffect(() => {
+    console.log(twoSum([4, 11, 17, 25], 21));
+    console.log(twoSum([0, 1, 2, 2, 3, 5], 4));
+    console.log(twoSum([-1, 0], -1));
+  })
   return (
-    <div>Question3</div>
+    <div>This is for Question 3</div>
   )
 }
 
-export default Question3
+export default Question3;
