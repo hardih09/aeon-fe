62c1b5bfa6f6551aa63a79907af1e62db5612573
hardihtahir <hardih@strateqebs.onmicrosoft.com>
2023-09-20 14:32:55 +0800

    fix infinite loop

--- aeon-fe/app/answers/question3/page.js
+++ aeon-fe/app/answers/question3/page.js
@@ -21,7 +21,7 @@ function Question3 (props) {
     console.log(twoSum([4, 11, 17, 25], 21));
     console.log(twoSum([0, 1, 2, 2, 3, 5], 4));
     console.log(twoSum([-1, 0], -1));
-  })
+  }, [])
   return (
     <div>This is for Question 3</div>
   )
